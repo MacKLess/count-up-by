a98c5d1617fae1264bbcb2dc26c6a4d49b73af51
Linda Luu and LDMacKrell <ldmackrell@>
2017-08-15 11:15:59 -0700

    make the calculations function correctly and provide output. hooray.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,14 @@
 $(document).ready(function(){
-  $("#userInput").click(function(event){
-    event.preventDefault();
-  });
-  var result = [];
+  $("#userInput").submit(function(event){
   var countTo = parseInt($("input#countTo").val());
   var countBy = parseInt($("input#countBy").val());
+  var result = [];
 
   for (var index = 0; index <= countTo; index += countBy){
     result.push(index);
-    console.log(result);
   };
 
-  $("#outPut").show(result);
-  console.log(result);
-
+  $("#outPut").text(result);
+    event.preventDefault();
+  });
 });
