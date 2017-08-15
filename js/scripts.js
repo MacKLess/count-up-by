59c7fe3247a17757dc8380e9996705b2339ea264
Linda Luu and LDMacKrell <ldmackrell@>
2017-08-15 14:02:55 -0700

    fix functionality for all error potentials complete with alert messages.

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,20 @@ $(document).ready(function(){
   var countBy = parseInt($("input#countBy").val());
   var result = [];
 
-  for (var index = 0; index <= countTo; index += countBy){
-    result.push(index);
-  };
-
+  if (isNaN(countTo) || isNaN(countBy)) {
+    alert ("Please enter a real number to calculate!")
+    }
+  else if (countTo < 0 || countBy < 0){
+      alert ("Please enter a whole positive number to calculate!")
+    }
+    else if (countTo < countBy){
+        alert ("Check your #s! We can't divide a value by a larger number!")
+      }
+      else {
+        for (var index = 0; index <= countTo; index += countBy){
+          result.push(index);
+        }
+      }
   $("#outPut").text(result);
     event.preventDefault();
   });
